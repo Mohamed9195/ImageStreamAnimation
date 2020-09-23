d8c420727cf72cea71020ef725449e301933ae2d
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-09-23 17:55:58 +0200

    Initial Commit

---
+++ ImageStreamAnimation/ImageStreamAnimation.h
@@ -0,0 +1,19 @@
+//
+//  ImageStreamAnimation.h
+//  ImageStreamAnimation
+//
+//  Created by mohamed hashem on 9/23/20.
+//  Copyright © 2020 mohamed hashem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageStreamAnimation.
+FOUNDATION_EXPORT double ImageStreamAnimationVersionNumber;
+
+//! Project version string for ImageStreamAnimation.
+FOUNDATION_EXPORT const unsigned char ImageStreamAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageStreamAnimation/PublicHeader.h>
+
+
